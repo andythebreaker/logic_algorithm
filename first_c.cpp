c4884b4cc82d11fc2ad93e3b6372c796bac85fd1
andythebreaker <43373581+andythebreaker@users.noreply.github.com>
2019-04-04 19:08:09 +0800

    Update first_c.cpp

--- first_c.cpp
+++ first_c.cpp
@@ -1,5 +1,61 @@
 #include <iostream>
 using namespace std;
+string EQ(string input,int add){
+ string buf="";
+ for (int i = 0 ; i < add ;i++){
+  buf+="=";
+ }
+return (input+buf);
+}
+string KMAP(string input){
+ string buf="Kmap";
+ 
+return (input+buf);
+}
+string EN(string input){
+ string buf="\n";
+ 
+return (input+buf);
+}
+string SL(string input){
+ string buf="\\";
+ 
+return (input+buf);
+}
+string SP(string input,int add){
+ string buf="";
+ for (int i = 0 ; i < add ;i++){
+  buf+=" ";
+ }
+return (input+buf);
+}
+string MN(string input,int add){
+ string buf="";
+ for (int i = 0 ; i < add ;i++){
+  buf+="-";
+ }
+return (input+buf);
+}
+
+string PL(string input){
+ string buf="+";
+ 
+return (input+buf);
+}
+
+string MP3(string input,int add){
+ string buf="";
+ for (int i = 0 ; i < add ;i++){
+  buf+="---+";
+ }
+return (input+buf);
+}
+string rowS(string input){
+ string buf=" | ";
+ 
+return (input+buf);
+}
+
 string XO(int IO){
  if(IO==0){
    return "0";
@@ -11,7 +67,7 @@ string XO(int IO){
    return "Z";
  }
 }
-void plot2(int *input){
+string plot2(int *input){
   string output2;
   for(int i=0;i<5;i++){
     output2+="=";
@@ -93,9 +149,45 @@ for(int i=0;i<5;i++){
 
   }
   output2+="+\n";
+ 
   cout << output2;
-
+return (output2);
 }
+string plot3(int *input){
+ string buf="";
+ buf=SL(SP(EN(EQ(KMAP(EQ(buf,9)),9)),2));
+ buf+="AB|";
+ buf=SP(buf,15);
+ buf+="|";
+ buf=SP(EN(buf),1);
+ buf+="C";
+ buf=SP(SL(SP(buf,1)),1);
+ buf+="| 00 01 11 10|";
+ buf=SP(EN(MP3(PL(MN(EN(buf),5)),4)),3);
+ buf+="0 | ";
+ buf+=XO(input[0]);
+ buf=rowS(buf);
+ buf+=XO(input[2]);
+ buf=rowS(buf);
+ buf+=XO(input[6]);
+ buf=rowS(buf);
+ buf+=XO(input[4]);
+ buf+=" |";
+ buf=SP(EN(MP3(PL(MN(EN(buf),5)),4)),3);
+ buf+="1 | ";
+ buf+=XO(input[1]);
+ buf=rowS(buf);
+ buf+=XO(input[3]);
+ buf=rowS(buf);
+ buf+=XO(input[7]);
+ buf=rowS(buf);
+ buf+=XO(input[5]);
+ buf+=" |";
+ buf=SP(EN(MP3(PL(MN(EN(buf),5)),4)),3);
+ cout << buf;
+return (buf);
+}
+
 int main() 
 {
   int X22[]={1,0,-1,4};
