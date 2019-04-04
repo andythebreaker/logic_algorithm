67219d4da3f5c668e41e39ff16b0822dd19cec04
andythebreaker <43373581+andythebreaker@users.noreply.github.com>
2019-04-04 15:26:32 +0800

    Update first_c.cpp

--- first_c.cpp
+++ first_c.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 using namespace std;
-
-int main() 
-{
+void plot2(int *input){
   string output2;
   for(int i=0;i<5;i++){
     output2+="=";
@@ -34,7 +32,63 @@ int main()
     output2+="-";
 
   }
-  output2+="+";
+  output2+="+\n";
+  for(int i=0;i<3;i++){
+    output2+="0";
+
+  }
+  output2+="0 | ";
+  output2+=input[0];//00->0
+   output2+=" | ";
+  output2+=input[2];//10->2
+   output2+="| \n";
   cout << output2;
+}
+for(int i=0;i<5;i++){
+    output2+="-";
+
+  }
+   output2+="+";
+  for(int i=0;i<3;i++){
+    output2+="-";
+
+  }
+  output2+="+";
+  for(int i=0;i<3;i++){
+    output2+="-";
+
+  }
+  output2+="+\n";
+  for(int i=0;i<3;i++){
+    output2+="0";
+
+  }
+  output2+="0 | ";
+  output2+=input[1];
+   output2+=" | ";
+  output2+=input[3];
+   output2+="| \n";
+for(int i=0;i<5;i++){
+    output2+="-";
+
+  }
+   output2+="+";
+  for(int i=0;i<3;i++){
+    output2+="-";
+
+  }
+  output2+="+";
+  for(int i=0;i<3;i++){
+    output2+="-";
+
+  }
+  output2+="+\n";
+  cout << output2;
+
+}
+int main() 
+{
+  int X22=[1,2,3,4]
+  plot2(X22);
   return 0;
 }
