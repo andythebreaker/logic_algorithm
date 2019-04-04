1532e51c5480528df0b37b375799fe372b50cffc
e24074724@mail.ncku.edu.tw <e24074724@mail.ncku.edu.tw>
2019-04-04 12:08:07 +0800

    firstcom

---
+++ first_c.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  string output2;
+  for(int i=0;i<5;i++){
+    output2+="=";
+
+  }
+  output2+="Kmap";
+ for(int i=0;i<5;i++){
+    output2+="=";
+
+  }
+ output2+="  i A|";
+  cout << output2;
+  return 0;
+}
